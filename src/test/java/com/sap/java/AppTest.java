aea3d42399ad91b60cfbf26577ebcc24926ad3fb
amlan_jyoti <amlanjyoti1@gmail.com>
2020-08-27 02:53:10 +0530

    initial commit

---
+++ src/test/java/com/sap/java/AppTest.java
@@ -0,0 +1,14 @@
+package com.sap.java;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void test1()
+    {
+        assertEquals(12, 6*2);
+    }
+}
