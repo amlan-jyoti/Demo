39e6c0a8d34048697e0dc27ce866fb8192319a12
amlan_jyoti <amlanjyoti1@gmail.com>
2020-08-27 21:07:03 +0530

    initial commit

--- src/main/java/com/sap/java/App.java
+++ src/main/java/com/sap/java/App.java
@@ -2,10 +2,34 @@ package com.sap.java;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Hello World");
+    public static void main( String[] args ) {
+        // [{()}]
 
-    }
+        // expression = [{( )}]
 
+        // if encountered with any of opening braces [ , {, ( -> push to the stack
+
+        // check if stack is empty - if yes - return ---> symetrical
+
+        // if encountered with any of closing braces ] , }, ) -> pop from the stack
+        // check the popped out element - if it belongs to other class of braces -> return as invalid
+
+        // check the stack size
+        // if (stack size != 0) -> invalid else valid --> asymmetrical
+
+        // expression = [{( })]
+
+        // [ { (  ) } ] ]
+
+        // [[ { (  ) } ]
+
+
+
+
+
+
+
+
+
+        }
 }
